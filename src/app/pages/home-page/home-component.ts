6eed6c98d95cffb301a472b4bf8bdb6c5db144b9
Valeriia Melnikov <valery.medvedeva@gmail.com>
2023-08-16 19:06:27 +0300

    feat: get user name from Auth service

--- src/app/pages/home-page/home-component.ts
+++ src/app/pages/home-page/home-component.ts
@@ -9,9 +9,9 @@ export default class HomeComponent extends RouteComponent {
   public render(parent: HTMLElement): void {
     super.render(parent);
     // todo ask Alex how to fix order of render and wait for response from AuthService.getUser()
-    const userName = AuthService.getUser();
-    console.log('userName', userName);
-    if (userName) {
+    if (localStorage.getItem('user')) {
+      const userName = AuthService.user.firstName;
+      console.log('userName home page', userName);
       const text = userName ? `Hello, ${userName}` : 'Coming soon...';
       this.message = BaseComponent.renderElem(this.container, 'h2', ['todo-message'], text);
     }
