44fae3ae8db10de3a68898e3fbcf413d727d54ca
Valeriia Melnikov <valery.medvedeva@gmail.com>
2023-08-19 21:22:52 +0300

    feat: add ApiMessageHandler class

---
+++ src/app/shared/util/api-message-handler.ts
@@ -0,0 +1,29 @@
+import Toastify from 'toastify-js';
+
+type MessageType = 'fail' | 'success';
+
+export default class ApiMessageHandler {
+  private static createMessage(message: string, type: MessageType) {
+    return {
+      stopOnFocus: true,
+      text: message,
+      duration: 2000,
+      close: true,
+      className: `toastify-${type} toastify-top toastify-center`,
+    };
+  }
+
+  public static showMessage(message: string, type: MessageType) {
+    switch (message) {
+      case 'Failed to fetch':
+        Toastify(this.createMessage('No connection', 'fail')).showToast();
+        break;
+      case 'There is already an existing customer with the provided email.':
+        Toastify(this.createMessage('Customet with provided email exists, please login', 'fail')).showToast();
+        break;
+
+      default:
+        Toastify(this.createMessage(message, type)).showToast();
+    }
+  }
+}
