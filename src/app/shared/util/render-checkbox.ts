81c18d26e541fec2f4be96bc4403ab20f76a71d7
Valeriia Melnikov <valery.medvedeva@gmail.com>
2023-08-22 00:45:01 +0300

    feat: add class for select

--- src/app/shared/util/render-checkbox.ts
+++ src/app/shared/util/render-checkbox.ts
@@ -1,7 +1,7 @@
 import BaseComponent from '../view/base-component';
 
 function renderCheckbox(form: HTMLElement, id: string, type: string, labelText: string): HTMLInputElement {
-  const inputContainer = BaseComponent.renderElem(form, 'div', ['input-container']);
+  const inputContainer = BaseComponent.renderElem(form, 'div', ['input-checkbox-container']);
   const label = BaseComponent.renderElem(inputContainer, 'label', ['input-container__label'], labelText);
   if (label instanceof HTMLLabelElement) label.htmlFor = id;
   const input = BaseComponent.renderElem(
