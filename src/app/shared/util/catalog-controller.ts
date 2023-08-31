e51ebf72072d31312af4f1e9a21df781d118f32b
Mark Romanov <oxxo8898@gmail.com>
2023-09-01 00:12:00 +0200

    feat: add setFilteredItems method

--- src/app/shared/util/catalog-controller.ts
+++ src/app/shared/util/catalog-controller.ts
@@ -1,25 +1,32 @@
-import { ProductProjection } from '@commercetools/platform-sdk';
+import EventEmitter from './emitter';
+import CatalogService from '../../services/catalog-service';
+import PriceRange from '../types/price-range-type';
 
 class CatalogController {
   private activeCategories: string[] = [];
-  private priceRange = { min: 0, max: 1000 };
+  private priceRange!: PriceRange;
   private brands: string[] = [];
-  private items!: ProductProjection[];
+
+  constructor(private emitter: EventEmitter) {}
 
   public setActiveCaregoties(value: string | null): void {
-    if (value !== null) this.activeCategories.push(value);
+    if (value) this.activeCategories.push(value);
+    this.setFilteredItems();
   }
 
   public removeActiveCaregoties(value: string | null): void {
-    if (value !== null) this.activeCategories = this.activeCategories.filter((item) => item !== value);
+    if (value) this.activeCategories = this.activeCategories.filter((item) => item !== value);
+    this.setFilteredItems();
   }
 
   public setActiveBrands(value: string | null): void {
-    if (value !== null) this.brands.push(value);
+    if (value) this.brands.push(value);
+    this.setFilteredItems();
   }
 
   public removeActiveBrands(value: string | null): void {
-    if (value !== null) this.brands = this.brands.filter((item) => item !== value);
+    if (value) this.brands = this.brands.filter((item) => item !== value);
+    this.setFilteredItems();
   }
 
   public setMinPrice(value: number): void {
@@ -28,6 +35,30 @@ class CatalogController {
 
   public setMaxPrice(value: number): void {
     this.priceRange.max = value;
+  }
+
+  private setFilteredItems() {
+    if (this.activeCategories.length > 0 || this.brands.length > 0) {
+      const categoryPromises = this.activeCategories.map((categoryKey) => {
+        return CatalogService.getCaterogyIdByKey(categoryKey);
+      });
+
+      Promise.all(categoryPromises)
+        .then((categoriesIds) => {
+          CatalogService.getProducts(categoriesIds, this.brands).then((res) => {
+            this.emitter.emit('updateCards', res);
+            if (this.activeCategories.length > 0 && this.brands.length === 0) this.emitter.emit('updateBrands', res);
+          });
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    } else {
+      CatalogService.getProducts().then((res) => {
+        this.emitter.emit('updateCards', res);
+        this.emitter.emit('updateBrands', res);
+      });
+    }
   }
 }
 
