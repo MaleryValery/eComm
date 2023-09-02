214f09c718501e460575592fffe9674fcf53d988
Valeriia Melnikov <valery.medvedeva@gmail.com>
2023-09-02 16:54:46 +0300

    fix: correct slider, names

--- src/app/pages/product-page/product-component.ts
+++ src/app/pages/product-page/product-component.ts
@@ -1,6 +1,5 @@
 import { Product, Image } from '@commercetools/platform-sdk';
 import 'swiper/css/bundle';
-import ProductService from '../../services/products-service';
 import BaseComponent from '../../shared/view/base-component';
 import RouteComponent from '../../shared/view/route-component';
 import './product-component.scss';
@@ -12,66 +11,76 @@ class ProductComponent extends RouteComponent {
   private cardWrapper!: HTMLElement;
   private productImgs!: Image[];
   private productsWrapper!: HTMLElement;
+  private productPopup!: HTMLElement;
   private productName!: HTMLElement;
   private swiperWrapper!: HTMLElement;
   private modalContainer!: HTMLElement;
   private fullPrice!: HTMLElement;
-  private discountedPrice!: HTMLElement;
 
   public async render(parent: HTMLElement): Promise<void> {
     super.render(parent);
     this.container.classList.add('product-route');
+
     this.productsWrapper = BaseComponent.renderElem(parent, 'div', ['product-wrapper']);
-    this.renderProductCard(await ProductService.getProduct('KEYAH0003'));
+    this.productPopup = BaseComponent.renderElem(parent, 'div', ['product-popup-container']);
     this.container.append(this.productsWrapper);
   }
 
-  public renderProductCard(card: Product): void {
+  public renderProductCard(product: Product): void {
+    this.productsWrapper.innerHTML = '';
     this.productName = BaseComponent.renderElem(
       this.productsWrapper,
       'p',
       ['product-name'],
-      card.masterData.current.name.en
+      product.masterData.current.name.en
     );
-    this.cardWrapper = BaseComponent.renderElem(this.productsWrapper, 'div', ['product-card']);
-
-    if (card.key) this.renderTextContentProductCard(card);
+    this.cardWrapper = BaseComponent.renderElem(this.productsWrapper, 'div', ['product-card-container']);
+    this.cardWrapper.innerHTML = '';
+    if (product.key) this.renderTextContentProductCard(product);
     this.renderImages(this.cardWrapper, 'regular');
-    this.renderImagesPopup(document.body);
+    this.renderImagesPopup();
   }
 
-  public async renderTextContentProductCard(card: Product): Promise<void> {
-    this.productImgs = card.masterData.current.masterVariant.images as Image[];
-    if (this.productImgs.length > 1) await loadSwiper();
+  public async renderTextContentProductCard(product: Product): Promise<void> {
+    this.productImgs = product.masterData.current.masterVariant.images as Image[];
+    await loadSwiper();
     const textContentWrapper = BaseComponent.renderElem(this.cardWrapper, 'div', ['text-content-wrapper']);
-    BaseComponent.renderElem(textContentWrapper, 'p', ['product-description'], card.masterData.current.description?.en);
+    BaseComponent.renderElem(
+      textContentWrapper,
+      'p',
+      ['product-description'],
+      product.masterData.current.description?.en
+    );
     const productSpecWrapper = BaseComponent.renderElem(textContentWrapper, 'ul', ['text-spec_list']);
-    if (card.masterData.current.masterVariant.attributes && card.masterData.current.masterVariant.attributes.length) {
-      card.masterData.current.masterVariant.attributes.forEach((attribute) => {
+    if (
+      product.masterData.current.masterVariant.attributes &&
+      product.masterData.current.masterVariant.attributes.length
+    ) {
+      product.masterData.current.masterVariant.attributes.forEach((attribute) => {
         productSpecWrapper.insertAdjacentHTML(
           'beforeend',
           `<li class = 'text-spec_element'><strong>${attribute.name}: </strong> <span>${attribute.value}</span></li>`
         );
       });
     }
-    this.renderPrices(card, textContentWrapper);
+    this.renderPrices(product, textContentWrapper);
   }
 
-  public async renderPrices(card: Product, parent: HTMLElement): Promise<void> {
+  public renderPrices(product: Product, parent: HTMLElement): void {
     const pricesContainer = BaseComponent.renderElem(parent, 'div', ['prices-content-wrapper']);
-    if (card.masterData.current.masterVariant.prices && card.masterData.current.masterVariant.prices.length) {
+    if (product.masterData.current.masterVariant.prices && product.masterData.current.masterVariant.prices.length) {
       this.fullPrice = BaseComponent.renderElem(
         pricesContainer,
         'div',
         ['full-price'],
         `${new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(
-          card.masterData.current.masterVariant.prices[0].value.centAmount / 100
+          product.masterData.current.masterVariant.prices[0].value.centAmount / 100
         )} `
       );
     }
     if (
-      card.masterData.current.masterVariant.prices?.length &&
-      card.masterData.current.masterVariant.prices[0].discounted
+      product.masterData.current.masterVariant.prices?.length &&
+      product.masterData.current.masterVariant.prices[0].discounted
     ) {
       this.fullPrice.classList.add('old-full-price');
       BaseComponent.renderElem(
@@ -79,7 +88,7 @@ class ProductComponent extends RouteComponent {
         'div',
         ['discounted-price'],
         `${new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(
-          card.masterData.current.masterVariant.prices[0].discounted.value.centAmount / 100
+          product.masterData.current.masterVariant.prices[0].discounted.value.centAmount / 100
         )}`
       );
     }
@@ -87,6 +96,7 @@ class ProductComponent extends RouteComponent {
 
   public renderImages(parent: HTMLElement, type = 'regular'): void {
     this.swiperWrapper = BaseComponent.renderElem(parent, 'div', ['swiper-container', `${type}`]);
+    this.swiperWrapper.innerHTML = '';
 
     this.swiperWrapper.addEventListener('click', (e) => {
       this.openImagesPopup(e, this.modalContainer);
@@ -121,12 +131,13 @@ class ProductComponent extends RouteComponent {
     );
   }
 
-  public renderImagesPopup(parent: HTMLElement): void {
-    this.modalContainer = BaseComponent.renderElem(parent, 'div', ['popup-wrapper', 'hide']);
+  public renderImagesPopup(): void {
+    this.productPopup.innerHTML = '';
+    this.modalContainer = BaseComponent.renderElem(this.productPopup, 'div', ['popup-wrapper', 'hide']);
     const closeBtn = BaseComponent.renderElem(this.modalContainer, 'div', ['close-btn']);
 
-    closeBtn.addEventListener('click', (e) => {
-      this.closeImagesPopup(e, this.modalContainer);
+    closeBtn.addEventListener('click', () => {
+      this.closeImagesPopup(this.modalContainer);
     });
     this.renderImages(this.modalContainer, 'zoom');
   }
@@ -139,9 +150,7 @@ class ProductComponent extends RouteComponent {
     }
   }
 
-  public closeImagesPopup(event: Event, popup: HTMLElement) {
-    const targetEvent = event.target as HTMLElement;
-    console.log(targetEvent);
+  public closeImagesPopup(popup: HTMLElement) {
     if (!popup.classList.contains('hide')) {
       popup.classList.add('hide');
       document.body.classList.remove('no-scroll');
