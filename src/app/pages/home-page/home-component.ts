832c67e88b7fc34064ba741b9bd73e4b7261d377
Valeriia Melnikov <valery.medvedeva@gmail.com>
2023-09-04 14:03:24 +0300

    feat: add nice home page

--- src/app/pages/home-page/home-component.ts
+++ src/app/pages/home-page/home-component.ts
@@ -1,4 +1,3 @@
-import ProductService from '../../services/products-service';
 import Router from '../../shared/util/router';
 import BaseComponent from '../../shared/view/base-component';
 import RouteComponent from '../../shared/view/route-component';
@@ -6,33 +5,45 @@ import RouteComponent from '../../shared/view/route-component';
 import './home-component.scss';
 
 export default class HomeComponent extends RouteComponent {
-  message!: HTMLElement;
+  public gitarCategory!: HTMLElement;
+  public ampCategory!: HTMLElement;
+  public accCategory!: HTMLElement;
+  public categoriesWrapper!: HTMLElement;
+  public catalogBtn!: HTMLElement;
 
-  public async render(parent: HTMLElement): Promise<void> {
+  public render(parent: HTMLElement): void {
     super.render(parent);
 
-    this.container.classList.add('home-route');
+    BaseComponent.renderElem(this.container, 'h2', ['heading-categories'], 'Welocome');
+    BaseComponent.renderElem(
+      this.container,
+      'h3',
+      ['subheading-categories'],
+      'We have many wonderful products for you.'
+    );
+    BaseComponent.renderElem(this.container, 'h3', ['subheading-categories'], 'You can find them in our catalog.');
+    this.catalogBtn = BaseComponent.renderElem(this.container, 'button', ['button-catalog'], "Let's go to the catalog");
+    this.categoriesWrapper = BaseComponent.renderElem(this.container, 'div', ['categories-cantainer']);
 
-    const wrapper = BaseComponent.renderElem(this.container, 'div', ['products-wrapper']);
-    wrapper.innerHTML = await this.addItems();
-    wrapper.addEventListener('click', (e) => {
-      this.cliсk(e);
-    });
+    this.gitarCategory = BaseComponent.renderElem(this.categoriesWrapper, 'div', ['category-item']);
+    this.ampCategory = BaseComponent.renderElem(this.categoriesWrapper, 'div', ['category-item']);
+    this.accCategory = BaseComponent.renderElem(this.categoriesWrapper, 'div', ['category-item']);
+
+    this.renderCategory(this.gitarCategory, 'Guitars', '../../../assets/img/guitar-categ.png');
+    this.renderCategory(this.ampCategory, 'Amplifiers', '../../../assets/img/amp-categ.jpg');
+    this.renderCategory(this.accCategory, 'Accessories', '../../../assets/img/acc-categ.jpg');
+
+    this.bindEvents();
   }
 
-  private async addItems(): Promise<string> {
-    const products = await ProductService.getAllProducts();
-    let items = '';
-    products.forEach((product) => {
-      items += `<div data-key = ${product.key}>${product.masterData.current.name.en}</div>`;
-    });
-    return items;
+  public renderCategory(parent: HTMLElement, header: string, imgSrc: string): void {
+    const imgContainer = BaseComponent.renderElem(parent, 'picture', ['category-img-container']);
+    const img = BaseComponent.renderElem(imgContainer, 'img', ['category-img']) as HTMLImageElement;
+    img.src = imgSrc;
+    BaseComponent.renderElem(parent, 'h3', ['category-heading'], `${header}`);
   }
 
-  public async cliсk(e: Event) {
-    const eventTarget = e.target as HTMLElement;
-    if (eventTarget.dataset.key) {
-      Router.navigate(`/catalog/${eventTarget.dataset.key}`);
-    }
+  public bindEvents() {
+    this.catalogBtn.addEventListener('click', Router.navigate('/catalog'));
   }
 }
