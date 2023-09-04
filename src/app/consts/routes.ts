5b55fc4973089bdc764e4381761d703e84d758d6
Valeriia Melnikov <valery.medvedeva@gmail.com>
2023-09-04 13:45:06 +0300

    fix: get fixed router

--- src/app/consts/routes.ts
+++ src/app/consts/routes.ts
@@ -1,31 +1,42 @@
 import ErrorComponent from '../pages/error-page/error-component';
 import HomeComponent from '../pages/home-page/home-component';
 import LoginComponent from '../pages/login-page/login-component';
+import ProductComponent from '../pages/product-page/product-component';
 import RegisterComponent from '../pages/register-page/register-component';
 import { Routes } from '../shared/types/routes-type';
 
 const ROUTS: Routes = [
   {
     name: 'Error',
-    path: '**',
+    path: /\*\*/,
     Component: ErrorComponent,
   },
   {
     name: 'Home',
-    path: '/',
+    path: /^\/$/,
     Component: HomeComponent,
   },
   {
     name: 'Login',
-    path: '/login',
+    path: /^\/login$/,
     Component: LoginComponent,
     authorizedRedirectPath: '/',
   },
   {
     name: 'Register',
-    path: '/register',
+    path: /^\/register$/,
     Component: RegisterComponent,
     authorizedRedirectPath: '/',
+  },
+  {
+    name: 'Product',
+    path: /^\/catalog\/key.+/,
+    Component: ProductComponent,
+  },
+  {
+    name: 'Catalog',
+    path: /^\/catalog$/,
+    Component: CatalogComponent,
   },
 ];
 
