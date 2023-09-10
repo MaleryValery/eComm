61c3fa38b1f7c1416f85a6b40dcf066a6d4b2734
Valeriia Melnikov <valery.medvedeva@gmail.com>
2023-09-10 17:30:17 +0300

    feat: add cart route

--- src/app/consts/routes.ts
+++ src/app/consts/routes.ts
@@ -1,3 +1,4 @@
+import CartComponent from '../pages/cart-page/cart-component';
 import CatalogComponent from '../pages/catalog-page/catalog-component';
 import ErrorComponent from '../pages/error-page/error-component';
 import HomeComponent from '../pages/home-page/home-component';
@@ -45,6 +46,11 @@ const ROUTS: Routes = [
     path: /^\/profile$/,
     Component: ProfileComponent,
     nonAuthorizedRedirectPath: '/',
+  },
+  {
+    name: 'Cart',
+    path: /^\/cart$/,
+    Component: CartComponent,
   },
 ];
 
