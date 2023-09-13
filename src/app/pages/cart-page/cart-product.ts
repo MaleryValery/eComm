0520176dde0cba402a5e27e51af95b9df01c04d0
Valeriia Melnikov <valery.medvedeva@gmail.com>
2023-09-13 17:14:15 +0300

    feat: add controllers for cart qty, update cart method

--- src/app/pages/cart-page/cart-product.ts
+++ src/app/pages/cart-page/cart-product.ts
@@ -1,18 +1,27 @@
+import { Cart } from '@commercetools/platform-sdk';
 import BaseComponent from '../../shared/view/base-component';
 import ProductCard from '../../shared/types/product-card-type';
 
 class CartProductComponent extends BaseComponent {
   public productCartWrapper!: HTMLElement;
   public removeFromCart!: HTMLButtonElement;
+  public increaseQty!: HTMLButtonElement;
+  public decreaseQty!: HTMLButtonElement;
   public priceProduct!: HTMLElement;
+  public currentItemQty!: HTMLElement;
+
+  private subscriptions() {
+    this.emitter.subscribe('updateQty', (itemId: string) => this.updateCart(itemId));
+  }
 
   public render(parent: HTMLElement, product: ProductCard) {
+    this.subscriptions();
     this.productCartWrapper = BaseComponent.renderElem(parent, 'div', ['cart-product-wrapper']);
     const productImgWrapper = BaseComponent.renderElem(this.productCartWrapper, 'div', ['cart-product__img-wrapper']);
     const productContentWrapper = BaseComponent.renderElem(this.productCartWrapper, 'div', [
       'cart-product__content-wrapper',
     ]);
-    const productQtyWrapper = BaseComponent.renderElem(this.productCartWrapper, 'div', ['cart-product__qty-wrapper']);
+    this.renderCartQty(product);
     const productPriceWrapper = BaseComponent.renderElem(this.productCartWrapper, 'div', [
       'cart-product__price-wrapper',
     ]);
@@ -43,25 +52,59 @@ class CartProductComponent extends BaseComponent {
         )}`
       );
     }
-    BaseComponent.renderElem(
-      productQtyWrapper,
-      'h3',
-      ['cart-product__qty', 'text-head-s'],
-      product.qtyInCart?.toString()
-    );
-    this.removeFromCart = BaseComponent.renderElem(
-      productQtyWrapper,
-      'button',
-      ['remove-from-cart'],
-      'remove'
-    ) as HTMLButtonElement;
-    this.removeFromCart.dataset.key = product.itemId;
+
     this.priceProduct = BaseComponent.renderElem(
       productPriceWrapper,
       'h3',
       ['cart-product__price', 'text-head-s'],
       `${(product.priceInCart ?? 0) / 100} €`
     );
+  }
+
+  private renderCartQty(product: ProductCard) {
+    const productQtyWrapper = BaseComponent.renderElem(this.productCartWrapper, 'div', ['cart-product__qty-wrapper']);
+    const manageQtyWrapper = BaseComponent.renderElem(productQtyWrapper, 'div', ['cart-product__manage-qty']);
+    this.decreaseQty = BaseComponent.renderElem(
+      manageQtyWrapper,
+      'button',
+      ['decr-from-cart'],
+      '-'
+    ) as HTMLButtonElement;
+    this.currentItemQty = BaseComponent.renderElem(
+      manageQtyWrapper,
+      'h3',
+      ['cart-product__qty', 'text-head-s'],
+      product.qtyInCart?.toString()
+    );
+    this.increaseQty = BaseComponent.renderElem(manageQtyWrapper, 'button', ['incr-to-cart'], '+') as HTMLButtonElement;
+
+    this.removeFromCart = BaseComponent.renderElem(
+      productQtyWrapper,
+      'button',
+      ['remove-from-cart'],
+      'remove'
+    ) as HTMLButtonElement;
+    this.removeFromCart.dataset.id = product.itemId;
+    this.removeFromCart.dataset.key = product.itemKey;
+    this.removeFromCart.dataset.lineItem = product.itemIdInCart;
+    this.decreaseQty.dataset.id = product.itemId;
+    this.decreaseQty.dataset.key = product.itemKey;
+    this.decreaseQty.dataset.lineItem = product.itemIdInCart;
+    this.increaseQty.dataset.id = product.itemId;
+    this.increaseQty.dataset.key = product.itemKey;
+    this.increaseQty.dataset.lineItem = product.itemIdInCart;
+    this.currentItemQty.dataset.id = product.itemId;
+    this.currentItemQty.dataset.key = product.itemKey;
+    this.currentItemQty.dataset.lineItem = product.itemIdInCart;
+  }
+
+  private updateCart(itemId: string) {
+    const cart = JSON.parse(localStorage.getItem('sntCart') as string) as Cart;
+    const itemInCart = cart.lineItems.find((item) => item.id === itemId);
+    if (itemInCart && this.currentItemQty.dataset.lineItem === itemId) {
+      this.currentItemQty.textContent = itemInCart.quantity.toString();
+      this.priceProduct.textContent = `${itemInCart.totalPrice.centAmount / 100} €`;
+    }
   }
 }
 
