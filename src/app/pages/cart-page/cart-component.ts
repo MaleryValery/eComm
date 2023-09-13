e77668c29ac135d1ec046d54db7864720dbe024b
Valeriia Melnikov <valery.medvedeva@gmail.com>
2023-09-13 17:11:40 +0300

    feat: add remove all btn

--- src/app/pages/cart-page/cart-component.ts
+++ src/app/pages/cart-page/cart-component.ts
@@ -7,8 +7,16 @@ import CartService from '../../services/cart-service';
 class CartComponent extends RouteComponent {
   private cartHeader!: HTMLElement;
   private cartBody!: HTMLElement;
+  public removeAllItems!: HTMLButtonElement;
 
   private cartListProductsComponent = new CartListProductsComponent(this.emitter);
+
+  public bindEvents() {
+    this.removeAllItems.addEventListener('click', () => {
+      CartService.removeAllItemsFromCart();
+      this.emitter.emit('renderEmptyCart', null);
+    });
+  }
 
   public render(parent: HTMLElement): void {
     super.render(parent);
@@ -17,18 +25,26 @@ class CartComponent extends RouteComponent {
       this.container,
       'div',
       ['cart-header', 'text-head-m'],
-      'Thank you for your choise!'
+      'Welcome to your cart 🛒'
     );
     this.cartBody = BaseComponent.renderElem(this.container, 'div', ['cart-body']);
 
     this.cartListProductsComponent.render(this.cartBody);
+    this.removeAllItems = BaseComponent.renderElem(
+      this.cartBody,
+      'button',
+      ['remove-all', 'text-head-s'],
+      'remove all products'
+    ) as HTMLButtonElement;
+
+    this.bindEvents();
   }
 
   public async show(): Promise<void> {
     super.show();
     try {
       await CartService.getUserCart();
-      this.emitter.emit('renderCarts', null);
+      this.emitter.emit('renderCart', null);
     } catch (err) {
       if ((err as Response).status !== 404) {
         this.emitter.emit('showErrorPage', null);
